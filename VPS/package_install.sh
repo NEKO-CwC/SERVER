a45ff2a5dab573c80c4cc536f89a81d899c83e80
NEKO-CwC <neeeeeeeeekooooooooo@gmail.com>
2025-07-10 09:33:34 +0800

    fixx

--- VPS/package_install.sh
+++ VPS/package_install.sh
@@ -12,34 +12,6 @@ log_info() {
 
 log_warn() {
     echo -e "${YELLOW}[WARN]${NC} $1" >&2
-    
-}
-
-log_error() {
-    echo -e "${RED}[ERROR]${NC} $1" >&2
-}
-
-log_step() {
-    echo -e "${BLUE}[STEP]${NC} $1" >&2
-}
-
-error_exit() {
-    log_error "$1"
-    exit 1
-}
-
-readonly GREEN='\033[0;32m'
-readonly YELLOW='\033[0;33m'
-readonly RED='\033[0;31m'
-readonly BLUE='\033[0;34m'
-readonly NC='\033[0m' # No Color
-
-log_info() {
-    echo -e "${GREEN}[INFO]${NC} $1" >&2
-}
-
-log_warn() {
-    echo -e "${YELLOW}[WARN]${NC} $1" >&2
 }
 
 log_error() {
